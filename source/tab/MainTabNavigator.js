adf3fb3fd1553e6885d18df698259c2d7ca7b2a7
tout_yong <twotell75@gmail.com>
2021-07-04 21:40:22 +0200

    The whole app is a tab navigator.

---
+++ source/tab/MainTabNavigator.js
@@ -0,0 +1,50 @@
+import Ionicons from "react-native-vector-icons/Ionicons";
+import React from "react";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import SimpleView from "../views/SimpleView";
+import HomeTab from "./HomeTab";
+
+const Tab = createBottomTabNavigator();
+
+export default function MainTabNavigator(){
+    return(
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = 'home-outline'
+                    }else if (route.name === 'Orders') {
+                        iconName = 'restaurant-outline';
+                    }else if (route.name === 'Archives') {
+                        iconName = 'archive-outline';
+                    }else if (route.name === 'Pending') {
+                        iconName = 'hourglass-outline';
+                    }else if (route.name === 'Settings') {
+                        iconName = 'settings-outline';
+                    }
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarButton: [
+
+                ].includes(route.name)
+                    ? () => {
+                        return null;
+                    }
+                    : undefined,
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Home" component={HomeTab} />
+            <Tab.Screen name="Orders" component={SimpleView}/>
+            <Tab.Screen name="Pending" component={SimpleView} />
+            <Tab.Screen name="Archives" component={SimpleView} />
+            <Tab.Screen name="Settings" component={SimpleView} />
+        </Tab.Navigator>
+    )
+}
