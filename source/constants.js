86226eee9a0021b1bfdf591ea3f40a2d86595b5d
tout_yong <twotell75@gmail.com>
2021-07-04 21:37:37 +0200

    To have a file which contains all string and avoid harcoded ones in code.

---
+++ source/constants.js
@@ -0,0 +1,16 @@
+let all_constants;
+export default all_constants = {
+    home:{
+        tab:{
+            new:{
+                title: 'Les nouveautés',
+            },
+            best:{
+                title: 'Les mieux notés',
+            },
+            famous:{
+                title: 'Les populaires',
+            }
+        }
+    }
+}
