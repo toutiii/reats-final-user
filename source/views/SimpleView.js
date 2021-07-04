b43bb159b6ae2de98df169953ff882f032574344
tout_yong <twotell75@gmail.com>
2021-07-04 21:40:56 +0200

    Useful dummy view in dev.

---
+++ source/views/SimpleView.js
@@ -0,0 +1,14 @@
+//Use that view when you just want to render something light.
+
+import React, {Component} from "react";
+import {Text, View} from "react-native";
+
+export default class SimpleView extends Component{
+    render(){
+        return(
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text> FREESTYLER </Text>
+            </View>
+        )
+    }
+}
