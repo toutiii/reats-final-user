b12b57230923e2d030a3cd240761fd1157f7c923
tout_yong <twotell75@gmail.com>
2021-07-24 16:51:08 +0200

    FlatList to display all new dishes.

--- source/flatlist/NewDishesFlatList.js
+++ source/flatlist/NewDishesFlatList.js
@@ -8,36 +8,39 @@ import {getNewDishesData} from "../api/fetch-home-data";
 
 
 export default function NewDishesFlatList({...props}) {
+
+    const dishItem = ({item}) => {
+        return (
+            <View style={styles_dish.dish_button_container}>
+                <TouchableHighlight
+                    onPress={() => {console.log('PRESSED!')}}
+                    style={{flex: 1}}
+                    underlayColor={all_constants.colors.inputBorderColor}
+                >
+                    <Dish
+                        key={item.id}
+                        dish_photo={item.photo}
+                        dish_name={item.dish_name}
+                        dish_category={item.dish_category}
+                        dish_rating={item.dish_rating}
+                        dish_price={item.dish_price + all_constants.currency_symbol}
+                        dish_description={item.dish_description}
+                        onPress={item.onPress}
+                    />
+                </TouchableHighlight>
+                <HorizontalLine/>
+            </View>
+        );
+    }
+
     return (
         <View style={{flex: 1}}>
             <FlatList
                 data={getNewDishesData()}
-                ListFooterComponent={<View></View>}
-                ListFooterComponentStyle={{borderWidth: 5, borderColor: 'red', borderRadius: 50}}
-                ListEmptyComponent={
-                    <View style={{flex: 1, alignItems: 'center'}}><Text style={{fontSize: 20}}>TEST</Text></View>
-                }
-                renderItem={({item}) => (
-                    <View style={styles_dish.dish_button_container}>
-                        <TouchableHighlight
-                            onPress={() => {console.log('PRESSED!')}}
-                            style={{flex: 1}}
-                            underlayColor={all_constants.colors.inputBorderColor}
-                        >
-                            <Dish
-                                key={item.id}
-                                dish_photo={item.photo}
-                                dish_name={item.dish_name}
-                                dish_category={item.dish_category}
-                                dish_rating={item.dish_rating}
-                                dish_price={item.dish_price + all_constants.currency_symbol}
-                                dish_description={item.dish_description}
-                                onPress={item.onPress}
-                            />
-                        </TouchableHighlight>
-                        <HorizontalLine/>
-                    </View>
-                )}
+                renderItem={dishItem}
+                keyExtractor={item => item.id}
+                onEndReached={getNewDishesData}
+                onEndReachedThreshold={0.8}
             />
         </View>
     )
