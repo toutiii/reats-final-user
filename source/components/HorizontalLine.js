55dabad92a13dc260259511937362ae1ecbbb0e4
tout_yong <twotell75@gmail.com>
2021-07-24 15:00:05 +0200

    Adding HorizontalLine component.

---
+++ source/components/HorizontalLine.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View} from "react-native";
+
+
+export default function HorizontalLine({...props}){
+    return (
+        <View
+            style={[
+                {borderBottomWidth: props.line_width ? props.line_width : 1},
+                {borderBottomColor: props.line_color ? props.line_color: 'black'}
+            ]}
+        />
+    )
+}
