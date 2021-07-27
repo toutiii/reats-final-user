7d52c5ce796d66120647e050f3ee5ae44d7f8614
tout_yong <twotell75@gmail.com>
2021-07-27 17:47:32 +0200

    Adding Preview component for FlatListSlider.

---
+++ source/components/Preview.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Image, Text, TouchableOpacity, View} from "react-native";
+import styles_preview from "../styles/styles-preview"
+
+export default function Preview ({item, imageKey, active,}) {
+    return (
+        <TouchableOpacity
+            style={styles_preview.videoContainer}
+            onPress={() => console.log('PRESSED')}
+        >
+            <View style={[styles_preview.imageContainer, styles_preview.shadow]}>
+                <Image
+                    style={[styles_preview.videoPreview, active ? {height: 200} : {height: 80}]}
+                    source={{uri: item[imageKey]}}
+                />
+            </View>
+            <Text numberOfLines={1} style={styles_preview.desc}>{item.dish_description}</Text>
+        </TouchableOpacity>
+    );
+};
