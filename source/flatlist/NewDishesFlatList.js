992e9379b13cfb8de70bfdacd6c33231a6c0202e
tout_yong <twotell75@gmail.com>
2021-07-27 17:33:05 +0200

    To increase UX with a dynamic home view.

--- source/flatlist/NewDishesFlatList.js
+++ source/flatlist/NewDishesFlatList.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
-import {FlatList, RefreshControl, TouchableHighlight, View} from "react-native";
-import styles_dish from '../styles/styles-dish'
+import {View} from "react-native";
 import all_constants from "../constants";
 import HorizontalLine from "../components/HorizontalLine";
 import Dish from "../components/Dish";
 import {getNewDishesData} from "../api/fetch-home-data";
+import {FlatListSlider} from "react-native-flatlist-slider";
+import DishItem from "../components/DishItem";
+import Preview from "../components/Preview";
 
 
 export default class NewDishesFlatList extends Component {
@@ -27,19 +29,25 @@ export default class NewDishesFlatList extends Component {
             this.setState({listdata: this.state.listdata.concat(results)})
         })
     }
+
     render() {
         return (
             <View style={{flex: 1}}>
-                <FlatList
-                    data={this.state.listdata}
-                    renderItem={this.dishItem}
-                    keyExtractor={item => item.id}
-                    onEndReached={getNewDishesData}
-                    onEndReachedThreshold={0.8}
-                    refreshControl={
-                        <RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh} />
-                    }
-                />
+                {
+                    this.state.listdata.length > 0 &&
+                    <FlatListSlider
+                        data={this.state.listdata}
+                        timer={5000}
+                        component={<Preview/>}
+                        indicatorActiveWidth={20}
+                        imageKey={'photo'}
+                        contentContainerStyle={{paddingHorizontal: 16}}
+                        width={all_constants.flatlist_slider_width}
+                        autoscroll={true}
+                        loop={false}
+                        animation={true}
+                    />
+                }
             </View>
         )
     }
