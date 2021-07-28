a40025768ff30b2bdef62ecc96f2f7cceb337114
tout_yong <twotell75@gmail.com>
2021-07-28 14:25:33 +0200

    Auto scroll was still too quick so we increased the timer.

--- source/views/HomeView.js
+++ source/views/HomeView.js
@@ -36,7 +36,7 @@ export default class HomeView extends Component {
                         this.state.listdata.length > 0 &&
                         <FlatListSlider
                             data={this.state.listdata}
-                            timer={5000}
+                            timer={7000}
                             component={<Preview/>}
                             indicatorActiveWidth={20}
                             imageKey={'photo'}
@@ -54,7 +54,7 @@ export default class HomeView extends Component {
                         this.state.listdata.length > 0 &&
                         <FlatListSlider
                             data={this.state.listdata}
-                            timer={6000}
+                            timer={8000}
                             component={<Preview/>}
                             indicatorActiveWidth={20}
                             imageKey={'photo'}
@@ -72,7 +72,7 @@ export default class HomeView extends Component {
                         this.state.listdata.length > 0 &&
                         <FlatListSlider
                             data={this.state.listdata}
-                            timer={7000}
+                            timer={9000}
                             component={<Preview/>}
                             indicatorActiveWidth={20}
                             imageKey={'photo'}
