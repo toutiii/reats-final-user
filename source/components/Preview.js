63d792aea0301cf1d24a4040c2a52ea20e05137e
tout_yong <twotell75@gmail.com>
2021-07-28 16:30:34 +0200

    The touchable part is restricted under the image to avoid unwanted press action by user.

--- source/components/Preview.js
+++ source/components/Preview.js
@@ -1,29 +1,34 @@
 import React from "react";
-import {Image, TouchableOpacity, View} from "react-native";
+import {Image, TouchableHighlight, TouchableOpacity, View} from "react-native";
 import styles_preview from "../styles/styles-preview"
 import all_constants from "../constants";
 import Dish from "./Dish";
 
 export default function Preview ({item, imageKey, active}) {
     return (
-        <TouchableOpacity
-            style={styles_preview.videoContainer}
-            onPress={() => console.log('PRESSED')}
-        >
+        <View style={styles_preview.videoContainer}>
             <View>
                 <Image
                     style={[styles_preview.videoPreview, active ? {height: 200, width: 350} : {height: 80}]}
                     source={{uri: item[imageKey]}}
                 />
             </View>
-            <Dish
-                dish_name={item.dish_name}
-                dish_category={item.dish_category}
-                dish_rating={item.dish_rating}
-                dish_price={item.dish_price + all_constants.currency_symbol}
-                dish_description={item.dish_description}
-                onPress={item.onPress}
-            />
-        </TouchableOpacity>
+            <TouchableHighlight
+                underlayColor={all_constants.colors.inputBorderColor}
+                style={{flex: 1}}
+                onPress={() => {
+                    console.log('PRESSED');
+                }}
+            >
+                <Dish
+                    dish_name={item.dish_name}
+                    dish_category={item.dish_category}
+                    dish_rating={item.dish_rating}
+                    dish_price={item.dish_price + all_constants.currency_symbol}
+                    dish_description={item.dish_description}
+                    onPress={item.onPress}
+                />
+            </TouchableHighlight>
+        </View>
     );
 };
