88ad2d2c106c8deec27b67ff4dae909ba8f44b16
tout_yong <twotell75@gmail.com>
2021-07-28 13:45:56 +0200

    Avoid hardcoded newsfeed titles.

--- source/constants.js
+++ source/constants.js
@@ -1,7 +1,11 @@
 let all_constants;
 export default all_constants = {
     home:{
-
+        news_feed_title:{
+            new: 'Les nouveautés',
+            top_rated: 'Les mieux notés',
+            famous: 'Les plus commandés'
+        }
     },
     currency_symbol: '€',
     rating_star: 'https://starpng.com/public/uploads/preview/yellow-star-transparent-background-png-101577029288c5hv8odvjm.png',
