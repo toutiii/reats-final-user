a276de45a1ee660f44e69a5b20100daadd1c6aff
tout_yong <twotell75@gmail.com>
2021-07-28 13:39:43 +0200

    The home view is not a tab navigator anymore.

--- source/constants.js
+++ source/constants.js
@@ -1,17 +1,7 @@
 let all_constants;
 export default all_constants = {
     home:{
-        tab:{
-            new:{
-                title: 'Les nouveautés',
-            },
-            best:{
-                title: 'Les mieux notés',
-            },
-            famous:{
-                title: 'Les populaires',
-            }
-        }
+
     },
     currency_symbol: '€',
     rating_star: 'https://starpng.com/public/uploads/preview/yellow-star-transparent-background-png-101577029288c5hv8odvjm.png',
