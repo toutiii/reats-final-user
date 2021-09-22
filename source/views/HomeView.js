27070dd57b888932ba9ecf0992ea23d92ff36f65
tout_yong <twotell75@gmail.com>
2021-09-22 17:29:20 +0200

    SafeAreaView will prevent unwanted overlap with system element.

--- source/views/HomeView.js
+++ source/views/HomeView.js
@@ -4,6 +4,7 @@ import all_constants from "../constants";
 import {getNewDishesData} from "../api/fetch-home-data";
 import {FlatListSlider} from "react-native-flatlist-slider";
 import Preview from "../components/Preview";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default class HomeView extends Component {
@@ -29,68 +30,70 @@ export default class HomeView extends Component {
 
     render() {
         return (
-            <ScrollView style={{flex: 1, marginTop: '10%'}}>
-                <View style={{flex: 1}}>
-                    <Text style={{marginLeft: '3%', fontSize: 22}}>
-                        {all_constants.home.news_feed_title.new}
-                    </Text>
-                    {
-                        this.state.listdata.length > 0 &&
-                        <FlatListSlider
-                            data={this.state.listdata}
-                            timer={7000}
-                            component={<Preview/>}
-                            indicatorActiveWidth={20}
-                            imageKey={'photo'}
-                            contentContainerStyle={{marginTop: '5%'}}
-                            width={all_constants.flatlist_slider_width}
-                            autoscroll={true}
-                            loop={false}
-                            animation={true}
-                        />
-                    }
-                </View>
-                <View style={{flex: 1, marginTop: '3%'}}>
-                    <Text style={{marginLeft: '3%', fontSize: 22}}>
-                        {all_constants.home.news_feed_title.top_rated}
-                    </Text>
-                    {
-                        this.state.listdata.length > 0 &&
-                        <FlatListSlider
-                            data={this.state.listdata}
-                            timer={8000}
-                            component={<Preview/>}
-                            indicatorActiveWidth={20}
-                            imageKey={'photo'}
-                            contentContainerStyle={{marginTop: '5%'}}
-                            width={all_constants.flatlist_slider_width}
-                            autoscroll={true}
-                            loop={false}
-                            animation={true}
-                        />
-                    }
-                </View>
-                <View style={{flex: 1, marginTop: '5%'}}>
-                    <Text style={{marginLeft: '3%', fontSize: 22}}>
-                        {all_constants.home.news_feed_title.famous}
-                    </Text>
-                    {
-                        this.state.listdata.length > 0 &&
-                        <FlatListSlider
-                            data={this.state.listdata}
-                            timer={9000}
-                            component={<Preview/>}
-                            indicatorActiveWidth={20}
-                            imageKey={'photo'}
-                            contentContainerStyle={{marginTop: '5%'}}
-                            width={all_constants.flatlist_slider_width}
-                            autoscroll={true}
-                            loop={false}
-                            animation={true}
-                        />
-                    }
-                </View>
-            </ScrollView>
+            <SafeAreaView style={{flex: 1}}>
+                <ScrollView style={{flex: 1, marginTop: '10%'}}>
+                    <View style={{flex: 1}}>
+                        <Text style={{marginLeft: '3%', fontSize: 22}}>
+                            {all_constants.home.news_feed_title.new}
+                        </Text>
+                        {
+                            this.state.listdata.length > 0 &&
+                            <FlatListSlider
+                                data={this.state.listdata}
+                                timer={7000}
+                                component={<Preview/>}
+                                indicatorActiveWidth={20}
+                                imageKey={'photo'}
+                                contentContainerStyle={{marginTop: '5%'}}
+                                width={all_constants.flatlist_slider_width}
+                                autoscroll={true}
+                                loop={false}
+                                animation={true}
+                            />
+                        }
+                    </View>
+                    <View style={{flex: 1, marginTop: '3%'}}>
+                        <Text style={{marginLeft: '3%', fontSize: 22}}>
+                            {all_constants.home.news_feed_title.top_rated}
+                        </Text>
+                        {
+                            this.state.listdata.length > 0 &&
+                            <FlatListSlider
+                                data={this.state.listdata}
+                                timer={8000}
+                                component={<Preview/>}
+                                indicatorActiveWidth={20}
+                                imageKey={'photo'}
+                                contentContainerStyle={{marginTop: '5%'}}
+                                width={all_constants.flatlist_slider_width}
+                                autoscroll={true}
+                                loop={false}
+                                animation={true}
+                            />
+                        }
+                    </View>
+                    <View style={{flex: 1, marginTop: '5%'}}>
+                        <Text style={{marginLeft: '3%', fontSize: 22}}>
+                            {all_constants.home.news_feed_title.famous}
+                        </Text>
+                        {
+                            this.state.listdata.length > 0 &&
+                            <FlatListSlider
+                                data={this.state.listdata}
+                                timer={9000}
+                                component={<Preview/>}
+                                indicatorActiveWidth={20}
+                                imageKey={'photo'}
+                                contentContainerStyle={{marginTop: '5%'}}
+                                width={all_constants.flatlist_slider_width}
+                                autoscroll={true}
+                                loop={false}
+                                animation={true}
+                            />
+                        }
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         )
     }
 }
