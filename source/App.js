1891b1038f17dcf6fa83b5aaf64ef16665f4dd6d
tout_yong <twotell75@gmail.com>
2021-09-22 17:31:30 +0200

    To use SafeAreView, we must set a SafeAreaProvider in root app.

--- source/App.js
+++ source/App.js
@@ -3,23 +3,26 @@ import { NavigationContainer } from '@react-navigation/native';
 import { AppearanceProvider } from 'react-native-appearance'
 import {createStackNavigator} from "@react-navigation/stack";
 import MainTabNavigator from "./tab/MainTabNavigator";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
 export default class App extends Component {
   render () {
     return (
-        <AppearanceProvider>
-          <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen
-                  name="MainTabNavigator"
-                  component={MainTabNavigator}
-                  options={{headerShown: false}}
-              />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </AppearanceProvider>
+        <SafeAreaProvider>
+            <AppearanceProvider>
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name="MainTabNavigator"
+                            component={MainTabNavigator}
+                            options={{headerShown: false}}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </AppearanceProvider>
+        </SafeAreaProvider>
     )
   }
 }
