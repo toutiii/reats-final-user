c8b0fa2b96f0ab109214cf932a99daa9cf03021f
tout_yong <twotell75@gmail.com>
2021-09-30 20:30:57 +0200

    Adding style for home view.

---
+++ source/styles/styles-home-view.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from "react-native"
+
+let styles_home_view;
+
+export default styles_home_view = StyleSheet.create({
+    activityIndicatorContainer: {
+        position: 'absolute',
+        flex: 1,
+        top: "40%",
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 2,
+    },
+})
