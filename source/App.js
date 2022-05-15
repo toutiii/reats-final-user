c595639c35c4b8da965bfa1b91b6c2b9a28bbd78
tout_yong <twotell75@gmail.com>
2022-05-15 22:51:18 +0200

    Raised an build error since we updated to react native SDR 44.

--- source/App.js
+++ source/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppearanceProvider } from 'react-native-appearance'
 import {createStackNavigator} from "@react-navigation/stack";
 import MainTabNavigator from "./tab/MainTabNavigator";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -11,17 +10,15 @@ export default class App extends Component {
   render () {
     return (
         <SafeAreaProvider>
-            <AppearanceProvider>
-                <NavigationContainer>
-                    <Stack.Navigator>
-                        <Stack.Screen
-                            name="MainTabNavigator"
-                            component={MainTabNavigator}
-                            options={{headerShown: false}}
-                        />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </AppearanceProvider>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="MainTabNavigator"
+                        component={MainTabNavigator}
+                        options={{headerShown: false}}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
         </SafeAreaProvider>
     )
   }
