c13b51c082267cd7da973dc2b6bae5f2df65c261
tout'it <70380468+toutiii@users.noreply.github.com>
2025-04-13 11:36:45 +0200

    [BUILD] Rework deploy (#6)
    Co-authored-by: Toutel NTATLA <twotell75@gmail.com>

--- source/app.config.js
+++ source/app.config.js
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: "reats-final-users",
         slug: "reats-final-users",
-        version: "1.0.0",
+        version: "0.1.0",
         orientation: "portrait",
         icon: "./assets/icon.png",
         splash: {
