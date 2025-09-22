f5cbaf6319e13d360a6466637839aba68d9cb2c8
DaveGlad <mdaveglad@gmail.com>
2025-09-22 05:57:59 +0100

    feat: add accordion and actionsheet components with gluestack UI integration

--- source/App.tsx
+++ source/App.tsx
@@ -3,13 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
+import { Button, ButtonText } from './components/ui/button';
 
 const App: React.FC = () => {
     return (
         
     <GluestackUIProvider mode="dark">
       <View style={styles.container}>
-            <Text>App</Text>
+            <Text >App</Text>
+            <Button >
+            <ButtonText>Click me</ButtonText>
+            </Button>
         </View>
     </GluestackUIProvider>
   
